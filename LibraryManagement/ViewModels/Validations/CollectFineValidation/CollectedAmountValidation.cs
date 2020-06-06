dc0f645f164f5e31b8ee0a88b11d0d7ed807a80a
Tuanle207 <letgo237@gmail.com>
2020-06-06 17:26:53 +0700

    Basically finish function of Collect Fine

--- LibraryManagement/ViewModels/Validations/CollectFineValidation/CollectedAmountValidation.cs
+++ LibraryManagement/ViewModels/Validations/CollectFineValidation/CollectedAmountValidation.cs
@@ -9,22 +9,22 @@ using System.Windows.Controls;
 
 namespace LibraryManagement.ViewModels
 {
-    public class Wrapper : DependencyObject
+    public class CollectedAmountValidationWrapper : DependencyObject
     {
-        public static readonly DependencyProperty maxAmount =
+        public static readonly DependencyProperty maxAmountProperty =
              DependencyProperty.Register("MaxAmount", typeof(int),
-             typeof(Wrapper), new FrameworkPropertyMetadata(int.MaxValue));
+             typeof(CollectedAmountValidationWrapper), new FrameworkPropertyMetadata(int.MaxValue));
 
         public int MaxAmount
         {
-            get { return (int)GetValue(maxAmount); }
-            set { SetValue(maxAmount, value); }
+            get { return (int)GetValue(maxAmountProperty); }
+            set { SetValue(maxAmountProperty, value); }
         }
     }
 
     public class CollectedAmountValidation : ValidationRule
     {
-        public Wrapper Wrapper { get; set; }
+        public CollectedAmountValidationWrapper Wrapper { get; set; }
 
         public CollectedAmountValidation()
         {
@@ -34,15 +34,18 @@ namespace LibraryManagement.ViewModels
         {
             int collectedAmount = 0;
 
-            if (((string)value).Contains('.') || ((string)value).Contains(','))
-            {
-                return new ValidationResult(false, "Số tiền thu phải là bội của 1000đ");
-            }
-
             try
             {
                 if (((string)value).Length > 0)
                     collectedAmount = Int32.Parse((String)value);
+                if (collectedAmount > this.Wrapper.MaxAmount)
+                {
+                    return new ValidationResult(false, "Số tiền thu không được vượt quá tổng nợ");
+                }
+                if (collectedAmount % 1000 != 0)
+                {
+                    return new ValidationResult(false, "Số tiền thu phải là bội của 1000đ");
+                }
             }
             catch (Exception)
             {
