c041976a7c2a525b8e9378c2a3e15371481087ab
Marcos240 <=>
2020-06-10 21:35:44 +0700

    Bug DB phan bao cao sach tra tre chua fix duoc

---
+++ LibraryManagement/ViewModels/ReportReturnLate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.ViewModels
+{
+    class ReportReturnLate : BaseViewModel
+    {
+        public int No { get; set; }
+        public string Name { get; set; }
+        public DateTime DateBorrow { get; set; }
+        public int DaysReturnLate { get; set; }
+    }
+}
