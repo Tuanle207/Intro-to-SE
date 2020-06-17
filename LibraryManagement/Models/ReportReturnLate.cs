0dab5ceedd5f27a632d9d3fb51187e0f99dff795
Marcos240 <=>
2020-06-17 17:52:10 +0700

    Tinh nang bao cao de lam giao dien, dang lam phan xuat excel

--- LibraryManagement/Models/ReportReturnLate.cs
+++ LibraryManagement/Models/ReportReturnLate.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using LibraryManagement.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibraryManagement.ViewModels
+namespace LibraryManagement.Models
 {
     class ReportReturnLate : BaseViewModel
     {
