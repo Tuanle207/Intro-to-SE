a13278c3b4a4dca2d6ee652f4149f02248c5aa60
Tuanle207 <letgo237@gmail.com>
2020-06-22 12:38:07 +0700

    Implement function of login, permission management

---
+++ LibraryManagement/Views/AddStaff.xaml.cs
@@ -0,0 +1,45 @@
+﻿using LibraryManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryManagement.Views
+{
+    /// <summary>
+    /// Interaction logic for AddStaff.xaml
+    /// </summary>
+    public partial class AddStaff : Window
+    {
+        public AddStaff()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PhoneNumberStaff_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void mPasswordStaff_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordStaff.Text = mPasswordStaff.Password;
+        }
+    }
+}
